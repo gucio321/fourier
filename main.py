91b599b59ce189e6379582a59f8d9f5324c4bef3
gucio321 <gucio321@protonmail.com>
2024-12-15 17:48:24 +0100

    edit script so that it really uses config

--- main.py
+++ main.py
@@ -24,18 +24,21 @@ t = 0.1 # czas odświeżania wykresu
 
 def main():
     x = np.linspace(0, 2*math.pi, precision)
-    y_org = (math.pi - x)/2
+    y_org = f(x)
     y = [a0]*precision
     plt.plot(x, y_org)
     myplot, = plt.plot(x, y)
     
-    for i in range(1,k+1):
-        y += np.sin(i*x)/i
-        myplot.set_ydata(y)
-        # add current i in legend
-        plt.legend(["f(x)" ,f'g(x, n = {i:.0f})'])
-        plt.draw()
-        plt.pause(t)
+    try:
+        for i in range(1,k+1):
+            y += fourier(x,i)
+            myplot.set_ydata(y)
+            # add current i in legend
+            plt.legend(["f(x)" ,f'g(x, n = {i:.0f})'])
+            plt.draw()
+            plt.pause(t)
+    except KeyboardInterrupt:
+        print("Exiting...")
 
 if __name__ == "__main__":
     main()
